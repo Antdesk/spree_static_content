f941d78351bbcec227309412c2345f0849c6db04
Magic <magic@antdesk>
2013-12-10 14:43:31 +0100

    json

--- app/controllers/spree/messages_controller.rb
+++ app/controllers/spree/messages_controller.rb
@@ -31,6 +31,7 @@ class Spree::MessagesController < Spree::StoreController
 
   def create
     @message = Spree::Message.new(params[:message])
+    render :json => @message and return
     if @message.valid?
       flash[:notice] = 'Wiadomosc wyslana! Dziekuje za poinformowanie nas.'
       @dane = {:email => "adrian.toczydlowski@gmail.com", :from_address => @message.email, :subject => @message.content}
